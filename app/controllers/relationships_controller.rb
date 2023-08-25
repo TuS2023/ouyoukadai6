dbcc1a65b5bdbd6c8a0b89a506183bc7e55690ad
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-08-25 07:19:41 +0000

    follow機能

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+class RelationshipsController < ApplicationController
+  def create
+    
+    
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def destroy
+    
+    
+    redirect_back(fallback_location: root_path)
+  end 
+end
