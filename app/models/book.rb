c6a1a2253fb490a8cac0fe61986dddd139ec3fb1
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-08-25 05:49:28 +0000

    [投稿で予期せぬエラー。答えと見比べのため投稿]

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,8 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
-  end 
+  end
 end
