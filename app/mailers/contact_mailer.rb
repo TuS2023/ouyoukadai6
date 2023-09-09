c2acc750a88a27e3e1afae7d2f07c6f7c0d6397c
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-09-09 03:22:28 +0000

    [fin-mailer]

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,7 @@
 class ContactMailer < ApplicationMailer
+  def send_email(mail_title,mail_content,Sgroup_users)
+    @mail_title = mail_title
+    @mail_content = mail_content
+    mail bcc: group_users.pluck(:email), subject: mail_title
+  end
 end
